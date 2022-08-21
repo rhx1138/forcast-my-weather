f11c79807e0855f8c2a0254bd199b36899bc3de6
rhx1138 <ryanjhendrix@gmail.com>
2022-08-21 13:13:57 -0400

    can now log city to console when running fetchWeather func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
-// let weather = {
-//     apiKey: "1b913796ee84a17f196943d065f7b698",
-//     fetchWeather: function() {
-//         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`) 
-//         .then((response) => response.json()) 
-//         .then((data) => console.log(data));
-// }
-// };
+// TODO: add city param to html
 
 let weather = {
     apiKey: "1b913796ee84a17f196943d065f7b698",
-    fetchWeather: function() {
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,daily&appid=${this.apiKey}`) 
+    fetchWeather: function(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${this.apiKey}`) 
         .then((response) => response.json()) 
         .then((data) => console.log(data));
-}
-};
+
+}};
 

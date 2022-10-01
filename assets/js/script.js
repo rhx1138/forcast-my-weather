c894a1bd2105a9c917c3246f72278b0f8c426180
rhx1138 <ryanjhendrix@gmail.com>
2022-10-01 09:59:04 -0400

    setting up new version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,49 @@
 
 
-let weather = {
-    apiKey: "1b913796ee84a17f196943d065f7b698",
-    fetchWeather: function (city) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${this.apiKey}`)
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
 
-    },
-    displayWeather: function (data) {
-        const {
-            name
-        } = data;
-        const {
-            icon
-        } = data.weather[0];
-        const {
-            temp,
-            humidity
-        } = data.main;
-        const {
-            speed
-        } = data.wind;
-        const {
-            lat,
-            lon
-        } = data.coord;
+
+
+
+// let weather = {
+//     apiKey: "1b913796ee84a17f196943d065f7b698",
+//     fetchWeather: function (city) {
+//         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${this.apiKey}`)
+//             .then((response) => response.json())
+//             .then((data) => this.displayWeather(data));
+
+//     },
+//     displayWeather: function (data) {
+//         const {
+//             name
+//         } = data;
+//         const {
+//             icon
+//         } = data.weather[0];
+//         const {
+//             temp,
+//             humidity
+//         } = data.main;
+//         const {
+//             speed
+//         } = data.wind;
+//         const {
+//             lat,
+//             lon
+//         } = data.coord;
                      
-        document.querySelector(".icon").src = `http://openweathermap.org/img/w/${icon}.png`;
-        document.querySelector(".city").innerText = `Weather in ${name}`;
-        document.querySelector(".temp").innerText = `${temp}°F`;
-        document.querySelector(".wind").innerText = `Wind speed: ${speed}mp/h`;
-        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
-        document.querySelector(".uv").innerText = `UV index: ${lon}%`;
+//         document.querySelector(".icon").src = `http://openweathermap.org/img/w/${icon}.png`;
+//         document.querySelector(".city").innerText = `Weather in ${name}`;
+//         document.querySelector(".temp").innerText = `${temp}°F`;
+//         document.querySelector(".wind").innerText = `Wind speed: ${speed}mp/h`;
+//         document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+//         document.querySelector(".uv").innerText = `UV index: ${lon}%`;
 
-        latitude = lat;
-        longitude = lon;
+//         latitude = lat;
+//         longitude = lon;
         
 
-    }
-};
+//     }
+// };
 
 
 
